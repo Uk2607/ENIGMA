dc1ebb71c1f9f02c83444014d13929a5a21c4ead
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-05 11:41:37 +0530

    look for first complete mismatched string

--- crack_enigma.cpp
+++ crack_enigma.cpp
@@ -1,1 +1,37 @@
-// Bombe machine
+#include<iostream>
+
+using namespace std;
+
+int firstCompleteMismatchedString(string enc, string str) {
+    if(enc.length()<=str.length()) return -1;
+    for(int i=0;i<=enc.length()-str.length();i++) {
+        bool flag = true;
+        for(int j=0;j<str.length();j++) {
+            if(enc[i+j] == str[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) return i;
+    }
+}
+
+int main() {
+    // Bombe machine
+    // cout<<"Enter encrypted strng: ";
+    string enc = "ORYMBXBKKHTMKTB";
+    // cin>>enc;
+    // cout<<"Enter the known part of the decrypted string: ";
+    string known_str = "NAMEIS";
+    // cin>>known_str;
+
+    int idx = firstCompleteMismatchedString(enc, known_str);
+    if(idx == -1) {
+        cout<<"Invalid known string\n";
+        return 0;
+    } else {
+        cout<<idx;
+    }
+
+    return 0;
+}
