3a43eed0d9954eea91cd07d3d7f8871c4eadfbcc
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-01-05 12:40:41 +0530

    base condition

--- crack_enigma.cpp
+++ crack_enigma.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
 int firstCompleteMismatchedString(string enc, string str) {
-    if(enc.length()<=str.length()) return -1;
+    int idx = -1;
     for(int i=0;i<=enc.length()-str.length();i++) {
         bool flag = true;
         for(int j=0;j<str.length();j++) {
@@ -12,25 +13,39 @@ int firstCompleteMismatchedString(string enc, string str) {
                 break;
             }
         }
-        if(flag) return i;
+        if(flag) {
+            if(idx!=-1) return -1;
+            idx = i;
+        }
     }
+    return idx;
 }
 
 int main() {
     // Bombe machine
-    // cout<<"Enter encrypted strng: ";
-    string enc = "ORYMBXBKKHTMKTB";
-    // cin>>enc;
+    ifstream fin("encoded.txt");
+    string enc;
+    fin>>enc;
+    cout<<enc<<"\n";
+    enc = "OMYYGNRNONEOXFD";
+
     // cout<<"Enter the known part of the decrypted string: ";
     string known_str = "NAMEIS";
     // cin>>known_str;
 
+    if(enc.length()<20 || known_str.length()<10) {
+        cout<<"Length of string is very small.\n";
+        return 0;
+    } else if(enc.length()<=known_str.length()) {
+        cout<<"Known string is longer than encoded string.\n";
+        return 0;
+    }
     int idx = firstCompleteMismatchedString(enc, known_str);
     if(idx == -1) {
-        cout<<"Invalid known string\n";
+        cout<<"Multiple matches for mismatch string.\n";
         return 0;
     } else {
-        cout<<idx;
+        cout<<idx<<"\n";
     }
 
     return 0;
